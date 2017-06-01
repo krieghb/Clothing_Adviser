66b00d4a73ccb6c5fb3d3f1c242b343be2782022
DKrieghbaum <Games4u2>
2017-06-01 14:22:38 -0500

    Quotes update

--- src/main/java/codingchallenge/docusign/ClothingAdviser.java
+++ src/main/java/codingchallenge/docusign/ClothingAdviser.java
@@ -15,14 +15,14 @@ import org.slf4j.LoggerFactory;
  *  COLD
  *  Comma separated list of numeric commands
  *  Command	Description	HOT Response	COLD Response
- *  1	Put on footwear	“sandals”	“boots”
- *  2	Put on headwear	“sun visor”	“hat”
- *  3	Put on socks	fail	“socks”
- *  4	Put on shirt	“t-shirt”	“shirt”
- *  5	Put on jacket	fail	“jacket”
- *  6	Put on pants	“shorts”	“pants”
- *  7	Leave house	“leaving house”	“leaving house”
- *  8	Take off pajamas	“Removing PJs”	“Removing PJs”
+ *  1	Put on footwear	"sandals"	"boots"
+ *  2	Put on headwear	"sun visor"	"hat"
+ *  3	Put on socks	fail	"socks"
+ *  4	Put on shirt	"t-shirt"	"shirt"
+ *  5	Put on jacket	fail	"jacket"
+ *  6	Put on pants	"shorts"	"pants"
+ *  7	Leave house	"leaving house"	"leaving house"
+ *  8	Take off pajamas	"Removing PJs"	"Removing PJs"
  *
  *  Rules:
  *    Initial state is in your house with your pajamas on
